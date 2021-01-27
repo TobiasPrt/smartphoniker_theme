c416a1514c14a10f049444bbe9bd65764615431f
Tobias Pörtner <t.poertner@outlook.com>
2021-01-27 11:41:13 +0100

    added some shell scripts

---
+++ act.sh
@@ -0,0 +1,2 @@
+#! bin/sh
+act --secret-file my.secrets
---
+++ update_demo.sh
@@ -0,0 +1,14 @@
+#! bin/bash
+
+branchname=$1
+
+if [[ -n "$branchname" ]]; then
+    echo "Attempt to switch to branch demo."
+    git checkout demo
+    echo "Attempting to merge $1 into branch demo."
+    git merge $1
+    echo "Attempting to push the changes made to branch demo."
+    git push origin demo
+else
+    echo "argument error"
+fi
