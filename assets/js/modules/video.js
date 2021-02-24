bc5e304333bd3dce04ab386360ee375b6db2d37f
Tobias Pörtner <t.poertner@outlook.com>
2021-02-24 18:02:11 +0100

    reordered functions

--- assets/js/modules/video.js
+++ assets/js/modules/video.js
@@ -7,6 +7,20 @@
 
 
 /**
+* Adds event listeners for all videos.
+*
+* @since 1.0.0
+*/
+export function addVideoEventListeners() {
+    const videos = document.querySelectorAll('.video');
+
+    videos && videos.forEach(video => {
+        video.addEventListener('click', () => loadVideo(video));
+    });
+}
+
+
+/**
  * Replaces video placeholder with iframe.
  *
  * @since 1.0.0
@@ -17,18 +31,4 @@ function loadVideo(video) {
     const videoLink = video.getAttribute('data-yt-id');
     video.classList.toggle('no-pseudo-elements');
     video.innerHTML = '<iframe src="' + videoLink + '" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-}
-
-
-/**
- * Adds event listeners for all videos.
- *
- * @since 1.0.0
- */
-export function addVideoEventListeners() {
-    const videos = document.querySelectorAll('.video');
-
-    videos && videos.forEach(video => {
-        video.addEventListener('click', () => loadVideo(video));
-    });
 }
