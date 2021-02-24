0a794b79e37804e42bd8ff26851e28e0d00254bd
Tobias Pörtner <t.poertner@outlook.com>
2021-02-24 17:43:24 +0100

    documented banner.js

--- assets/js/modules/banner.js
+++ assets/js/modules/banner.js
@@ -1,16 +1,35 @@
+/**
+ * ES6 Module: banner block
+ * 
+ * @package Smartphoniker
+ * @since 1.0.0
+ */
+
+
 const banner = document.getElementById('banner');
 const header = document.getElementById('header');
 const nav = document.getElementById('nav');
 
-function closeBanner() {
+
+/**
+ * Hides banner block.
+ *
+ * @since 1.0.0
+ */
+function hideBanner() {
     banner.classList.toggle("banner--isHidden");
+    // Adjust header size
     header.classList.toggle("header--bannerIsHidden");
+    // Adjust nav position
     nav.classList.toggle("nav__list--bannerIsHidden");
 }
 
 
-function addBannerEventListener() {
-    banner && banner.addEventListener('click', closeBanner)
+/**
+ * Adds event listener for banner block.
+ * 
+ * @since 1.0.0
+ */
+export function addBannerEventListener() {
+    banner && banner.addEventListener('click', hideBanner)
 }
-
-export { addBannerEventListener };
