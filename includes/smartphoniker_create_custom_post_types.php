f5b6766638490d753b78d7a3ffab88f69ffc331d
Tobias Pörtner <t.poertner@outlook.com>
2021-02-28 10:08:53 +0100

    removed permalinks and single pages for employees

--- includes/smartphoniker_create_custom_post_types.php
+++ includes/smartphoniker_create_custom_post_types.php
@@ -16,36 +16,39 @@ function smartphoniker_create_custom_post_types() {
 function smartphoniker_register_employee_post_type() {
     
     $labels = array(
-        'name'                => _x( 'Mitarbeiter', 'Post Type General Name' ),
-        'singular_name'       => _x( 'Mitarbeiter', 'Post Type Singular Name' ),
-        'menu_name'           => __( 'Mitarbeiter' ),
-        'all_items'           => __( 'Alle Mitarbeiter' ),
-        'view_item'           => __( 'Mitarbeiter anzeigen' ),
-        'add_new_item'        => __( 'Mitarbeiter hinzufügen' ),
-        'add_new'             => __( 'Neuer Mitarbeiter' ),
-        'edit_item'           => __( 'Mitarbeiter bearbeiten' ),
-        'update_item'         => __( 'Mitarbeiter aktualisieren' ),
-        'search_items'        => __( 'Mitarbeiter durchsuchen' ),
-        'not_found'           => __( 'keinen Mitarbeiter gefunden' ),
-        'not_found_in_trash'  => __( 'keinen gelöschten Mitarbeiter gefunden' ),
+        'name'                => _x( 'Mitarbeiter:innen', 'Post Type General Name' ),
+        'singular_name'       => _x( 'Mitarbeiter:in', 'Post Type Singular Name' ),
+        'menu_name'           => __( 'Mitarbeiter:innen' ),
+        'all_items'           => __( 'Alle Mitarbeiter:innen' ),
+        'view_item'           => __( 'Mitarbeiter:in anzeigen' ),
+        'add_new_item'        => __( 'Mitarbeiter:in hinzufügen' ),
+        'add_new'             => __( 'Mitarbeiter:in hinzufügen' ),
+        'edit_item'           => __( 'Mitarbeiter:in bearbeiten' ),
+        'update_item'         => __( 'Mitarbeiter:in aktualisieren' ),
+        'search_items'        => __( 'Mitarbeiter:in durchsuchen' ),
+        'not_found'           => __( 'keine Mitarbeiter:innen gefunden' ),
+        'not_found_in_trash'  => __( 'keine gelöschten Mitarbeiter:innen gefunden' ),
     );
 
     $options = array(
-        'label' => __( 'Mitarbeiter' ),
-        'description' => __( 'Alle Mitarbeiter von Smartphoniker.' ),
+        'label' => __( 'Mitarbeiter:innen' ),
+        'description' => __( 'Alle Mitarbeiter:innen von Smartphoniker.' ),
         'labels' => $labels,
         'supports' => array(
             'title', 
             'thumbnail'
         ),
         'hierarchical' => false,
-        'public' => true,
-        'show_in_nav_menu' => false,
         'menu_icon' => 'dashicons-groups',
         'has_archive' => true,
-        'rewrite' => array( 'slug' => 'employees' ),
         'show_in_rest' => true,
-        'taxonomies' => array( 'role' ),
+        'public' => false,
+        'publicly_queryable' => true,
+        'show_ui' => true,
+        'exclude_from_search' => true,
+        'show_in_nav_menus' => false,
+        'has_archive' => false,
+        'rewrite' => false,
     );
     
     register_post_type( 'employee', $options );
