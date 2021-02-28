8b0f5b9de96e767b44bad063d3703ba28fb10744
Tobias Pörtner <t.poertner@outlook.com>
2021-02-28 10:15:51 +0100

    splitted post types into several files

--- includes/smartphoniker_create_custom_post_types.php
+++ includes/smartphoniker_create_custom_post_types.php
@@ -9,47 +9,7 @@ add_action( 'init', 'smartphoniker_create_custom_post_types' );
 
 
 function smartphoniker_create_custom_post_types() {
-    smartphoniker_register_employee_post_type();
-}
-
-
-function smartphoniker_register_employee_post_type() {
-    
-    $labels = array(
-        'name'                => _x( 'Mitarbeiter:innen', 'Post Type General Name' ),
-        'singular_name'       => _x( 'Mitarbeiter:in', 'Post Type Singular Name' ),
-        'menu_name'           => __( 'Mitarbeiter:innen' ),
-        'all_items'           => __( 'Alle Mitarbeiter:innen' ),
-        'view_item'           => __( 'Mitarbeiter:in anzeigen' ),
-        'add_new_item'        => __( 'Mitarbeiter:in hinzufügen' ),
-        'add_new'             => __( 'Mitarbeiter:in hinzufügen' ),
-        'edit_item'           => __( 'Mitarbeiter:in bearbeiten' ),
-        'update_item'         => __( 'Mitarbeiter:in aktualisieren' ),
-        'search_items'        => __( 'Mitarbeiter:in durchsuchen' ),
-        'not_found'           => __( 'keine Mitarbeiter:innen gefunden' ),
-        'not_found_in_trash'  => __( 'keine gelöschten Mitarbeiter:innen gefunden' ),
-    );
-
-    $options = array(
-        'label' => __( 'Mitarbeiter:innen' ),
-        'description' => __( 'Alle Mitarbeiter:innen von Smartphoniker.' ),
-        'labels' => $labels,
-        'supports' => array(
-            'title', 
-            'thumbnail'
-        ),
-        'hierarchical' => false,
-        'menu_icon' => 'dashicons-groups',
-        'has_archive' => true,
-        'show_in_rest' => true,
-        'public' => false,
-        'publicly_queryable' => true,
-        'show_ui' => true,
-        'exclude_from_search' => true,
-        'show_in_nav_menus' => false,
-        'has_archive' => false,
-        'rewrite' => false,
-    );
-    
-    register_post_type( 'employee', $options );
+    foreach ( (array) glob( __DIR__ . "/custom_post_types/*.php" ) as $filename ) {
+        require_once $filename;
+    }
 }
