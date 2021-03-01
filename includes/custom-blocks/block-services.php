8b30a8447c6e229426baa5e2a42138eb3c3c4b17
Tobias Pörtner <t.poertner@outlook.com>
2021-03-01 16:40:09 +0100

    implemented option to choose services to display

--- includes/custom-blocks/block-services.php
+++ includes/custom-blocks/block-services.php
@@ -14,15 +14,38 @@ use Carbon_Fields\Block;
 use Carbon_Fields\Field;
 
 /**
- * Registers Gutenberg Block block2.
+ * Registers Gutenberg Block services.
  *
  * @since 1.0.0
  */
 (function() {
+
+    function get_services_list() {
+        $query = new WP_Query( array(
+            'post_type' => 'service',
+        ) );
+
+        $services = array();
+        if ($query->have_posts() ) {
+            while ( $query->have_posts() ) {
+                $query->the_post();
+                $services = array(
+                    get_the_id() => get_the_title()
+                );
+            };
+            wp_reset_postdata();
+        }
+        return $services;
+    }
+    
+
     Block::make( __( 'Services' ) )
         ->add_fields ( array(
             Field::make( 'html', 'description', __( 'Services Beschreibung' ) )
-                ->set_html( 'Hier wird eine 2-spaltige Liste von allen Services aus der Widget Gruppe ausgegeben.' )
+                ->set_html( 'Hier wird eine 2-spaltige Liste von den ausgewählten Services dargestellt.' ),
+            Field::make( 'set', 'services', __( 'Ausgewählte Services' ))
+                ->set_options( 'get_services_list' )
+                ->set_help_text( 'Zeigt das Icon, den Namen und die Kurzbeschreibung jedes ausgewählten Service an.' )
         ) )
         ->set_parent( 'carbon-fields/section' )
         ->set_render_callback( function ( array $fields, array $attributes, string $inner_blocks ) {
--- template-parts/component-services.php
+++ template-parts/component-services.php
@@ -5,43 +5,26 @@
  * @package Smartphoniker
  * @since 1.0.0
  */
-
-
-$services = new WP_Query( array(
-    'post_type' => 'service',
-    'meta_query' => array(
-        'group' => array(
-            'key' => 'group',
-            'value' => 'widget'
-        ),
-    ),
-) );
 ?>
 
 
 <div class="section__content columns-2">
 
-    <!-- columns of services -->
-    <?php if ($services->have_posts() ) : ?>
+    <!-- 1 Column per service -->
+    <?php foreach ( (array) $args['services'] as $service => $service_id ): ?>
 
-            <?php while ( $services->have_posts() ) : $services->the_post(); ?>
-                
-                <div class="columns-2__column">
-                    <?php echo wp_get_attachment_image( get_post_meta( get_the_ID(), '_icon' )[0], 'thumbnail', false, array( 'class' => 'columns-2__icon' ) ); ?>
-                    <div class="columns-2__contentwrapper">
-                        <h3 class="columns-2__heading">
-                            <?php the_title(); ?>
-                        </h3>
-                        <p class="columns-2__text">
-                            <?php echo get_post_meta( get_the_ID(), '_description' )[0]; ?>
-                        </p>
-                    </div>
+        <div class="columns-2__column">
+                <?php echo wp_get_attachment_image( get_post_meta( intval( $service_id ), '_icon' )[0], 'thumbnail', false, array( 'class' => 'columns-2__icon' ) ); ?>
+                <div class="columns-2__contentwrapper">
+                    <h3 class="columns-2__heading">
+                        <?php echo get_post_field( 'post_title', intval( $service_id ) ); ?>
+                    </h3>
+                    <p class="columns-2__text">
+                        <?php echo get_post_meta( intval( $service_id ), '_description' )[0]; ?>
+                    </p>
                 </div>
-            
-            <?php endwhile; ?>
-            <?php wp_reset_postdata(); ?>
+            </div>
 
-    <?php endif; ?>
-
-    
+    <?php endforeach; ?>
+   
 </div>
