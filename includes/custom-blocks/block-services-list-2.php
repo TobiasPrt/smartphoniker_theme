acd5657be229a5f9bbecd5ee92b0084361545ce1
Tobias Pörtner <t.poertner@outlook.com>
2021-03-02 08:13:31 +0100

    renamed smartphoniker service list, user now able to choose which services to display

--- includes/custom-blocks/block-services-list-2.php
+++ includes/custom-blocks/block-services-list-2.php
@@ -10,6 +10,9 @@
  */
 
 
+require_once __DIR__ . '/../smartphoniker_util.php';
+
+
 use Carbon_Fields\Block;
 use Carbon_Fields\Field;
 
@@ -19,36 +22,16 @@ use Carbon_Fields\Field;
  * @since 1.0.0
  */
 (function() {
-
-    function get_services_list() {
-        $query = new WP_Query( array(
-            'post_type' => 'service',
-        ) );
-
-        $services = array();
-        if ($query->have_posts() ) {
-            while ( $query->have_posts() ) {
-                $query->the_post();
-                $services = array(
-                    get_the_id() => get_the_title()
-                );
-            };
-            wp_reset_postdata();
-        }
-        return $services;
-    }
-    
-
-    Block::make( __( 'Services' ) )
+    Block::make( __( 'Services-List-2' ) )
         ->add_fields ( array(
             Field::make( 'html', 'description', __( 'Services Beschreibung' ) )
                 ->set_html( 'Hier wird eine 2-spaltige Liste von den ausgewählten Services dargestellt.' ),
             Field::make( 'set', 'services', __( 'Ausgewählte Services' ))
-                ->set_options( 'get_services_list' )
+                ->set_options( call_user_func( 'get_all_posts', 'service' ) )
                 ->set_help_text( 'Zeigt das Icon, den Namen und die Kurzbeschreibung jedes ausgewählten Service an.' )
         ) )
         ->set_parent( 'carbon-fields/section' )
         ->set_render_callback( function ( array $fields, array $attributes, string $inner_blocks ) {
-            get_template_part( 'template-parts/component', 'services', $fields );
+            get_template_part( 'template-parts/component', 'services-list-2', $fields );
         } );
 })();
