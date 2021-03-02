df3d877ea07cac57db8cd0558e16c8283bcfac72
Tobias Pörtner <t.poertner@outlook.com>
2021-03-02 08:12:41 +0100

    created single template

---
+++ single.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template: Single Post Type
+ *
+ * @package Smartphoniker
+ * @since 1.0.0
+ */
+
+get_header();
+
+if ( have_posts() ) {
+    while ( have_posts() ) {
+        the_post();
+        the_content();
+    }
+}
+
+get_footer();
