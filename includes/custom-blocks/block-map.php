1fc5ef33c63898e4ea19fe8dc84ea3c19c42b900
Tobias Pörtner <t.poertner@outlook.com>
2021-03-03 12:20:50 +0100

    added map component and block

---
+++ includes/custom-blocks/block-map.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Custom Block: Map
+ * 
+ * Gutenberg block which represents a map marked with locations.
+ *
+ * @package Smartphoniker
+ * @since 1.0.0
+ */
+
+
+require_once __DIR__ . '/../smartphoniker_util.php';
+
+
+use Carbon_Fields\Block;
+use Carbon_Fields\Field;
+
+/**
+ * Registers Gutenberg Block map.
+ *
+ * @since 1.0.0
+ */
+(function() {
+    Block::make( __( 'Map' ) )
+        ->add_fields( array(
+            Field::make( 'separator', 'separator', __( 'Karte' ) ),
+            Field::make( 'set', 'stores', __( 'Anzuzeigende Stores auswählen' ) )
+                ->set_options( call_user_func( 'get_all_posts', 'store' ) )
+        ) )
+        ->set_parent( 'carbon-fields/section' )
+        ->set_render_callback( function ( array $fields, array $attributes, string $inner_blocks ) {
+
+            $locations = array();
+            foreach( $fields['stores'] as $store => $store_id ) {
+                array_push( $locations, array(
+                    'id' => $store_id,
+                    'title' => get_post_field( 'post_title', $store_id ),
+                    'lat' => carbon_get_post_meta( intval( $store_id ), 'address' )['lat'],
+                    'lng' => carbon_get_post_meta( intval( $store_id ), 'address' )['lng'],
+                ) );
+            }
+
+            get_template_part( 'template-parts/component', 'map', array( 'locations' => $locations ) );
+        } );
+})();
---
+++ template-parts/component-map.php
@@ -0,0 +1,82 @@
+<?php 
+/**
+ * Template-Part: Map
+ * 
+ * @package Smartphoniker
+ * @since 1.0.0
+ */
+?>
+
+<div id="map" class="map"></div>
+<script>
+    function initMap() {
+        const mapCenter = {
+            lat: 54.327656,
+            lng: 10.0685555
+        };
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 8,
+            center: mapCenter,
+            styles: [
+                {
+                    featureType: "all",
+                    stylers: [
+                        { "saturation": -100 },
+                    ]
+                }
+            ],
+            streetViewControl: false,
+            fullscreenControl: false,
+            rotateControl: false,
+            mapTypeControl: false,
+        });
+
+        
+
+        <?php foreach ( (array) $args['locations'] as $location_index => $location ): ?>
+            
+            const marker<?php echo $location_index; ?> = new google.maps.Marker({
+                position: {
+                    lat: parseFloat('<?php echo $location['lat']; ?>'),
+                    lng: parseFloat('<?php echo $location['lng']; ?>')
+                },
+                map: map,
+                title: '<?php echo $location['title']; ?>',
+                icon: "<?php echo get_template_directory_uri(); ?>/assets/images/icons/map_needle_filled.png",
+            });
+
+            const infowindow<?php echo $location_index; ?> = new google.maps.InfoWindow({
+                content: `
+                    <div id="content">
+                        <div id="siteNotice">
+                            <h3 class="card__heading">
+                                <?php echo get_post_field( 'post_title', intval( $location['id'] ) ); ?>
+                            </h3>
+                            <p class="card__text">
+                                <?php echo str_replace( ',', '<br>', carbon_get_post_meta( intval( $location['id'] ), 'address' )['address'] ); ?>
+                            </p>
+                            <br>
+                            <p class="card__text">
+                                <?php echo get_post_meta( intval( $location['id'] ), '_opening_hours' )[0]; ?>
+                            </p>
+                            <a href="<?php echo get_permalink( $location['id'] ); ?>" class="card__link"
+                            ><img
+                                class="card__icon"
+                                src="<?php echo get_template_directory_uri(); ?>/assets/images/icons/arrowright_box.svg"
+                                alt="Arrow right"
+                            />mehr erfahren</a
+                            >
+                        </div>
+                    </div>
+                `,
+            });
+
+            marker<?php echo $location_index; ?>.addListener( 'click', () => {
+                infowindow<?php echo $location_index; ?>.open(map, marker<?php echo $location_index; ?>);
+            } );
+
+        <?php endforeach; ?>
+    }
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4QnNCsxxSj4yOixOksho0L4EP7vO-SuI&libraries=places&callback=initMap" async defer></script>
