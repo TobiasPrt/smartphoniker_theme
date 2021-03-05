7050c4912dd8ddef9797912175db77f3c491c652
Tobias Pörtner <t.poertner@outlook.com>
2021-03-05 18:02:49 +0100

    info text for long text

--- includes/custom-blocks/block-long-text.php
+++ includes/custom-blocks/block-long-text.php
@@ -20,7 +20,9 @@ use Carbon_Fields\Field;
 (function() {
     Block::make( __( 'long-text' ) )
         ->add_fields( array(
-             Field::make( 'rich_text', 'text', __( 'Formatierter Langer Text' ) )
+            Field::make( 'html', 'info', __( 'Info' ) )
+                ->set_html( 'Heading 3 für Überschriften wählen. Die Listenfunktion für Aufzählungen verwenden. Neue Zeilen innerhalb eines Paragraph mit Shift + Enter erzeugen.' ),
+            Field::make( 'rich_text', 'text', __( 'Formatierter Langer Text' ) )
         ) )
         ->set_parent( 'carbon-fields/section' )
         ->set_render_callback( function ( array $fields, array $attributes, string $inner_blocks ) {
