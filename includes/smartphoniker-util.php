f6705b57abf361b0055a97c41695ace49214243d
Tobias Pörtner <t.poertner@outlook.com>
2021-03-28 15:19:05 +0200

    fix: refactored code in get all posts function

--- includes/smartphoniker-util.php
+++ includes/smartphoniker-util.php
@@ -15,35 +15,30 @@
  * @return array format: [post-id] => [title]
  */
 function get_all_posts( $post_type = null, string $category = null ): array {
-    $post_list = get_posts( array(
-        'numberposts' => -1,
-        'post_type' => $post_type,
-        'post_status' => array('publish'),
-    ) );
-
+    // check passed arguments
     if ($post_type === null ) {
-        $post_list = get_posts(
-            array(
+        $post_list = get_posts( array(
             'numberposts' => -1,
-            'post_status' => array('publish'),
             'post_type' => get_post_types('', 'names'),
-            )
-        );
-    }
-
-    if ( $category !== null && $post_type !== null) {
+            'post_status' => array('publish'),
+            
+        ) );
+    } elseif ( $category !== null && $post_type !== null) {
         $post_list = get_posts( array(
             'post_type' => $post_type,
             'post_status' => array('publish'),
             'category' => $category,
         ) );
+    } else {
+        $post_list = get_posts( array(
+            'numberposts' => -1,
+            'post_type' => $post_type,
+            'post_status' => array('publish'),
+        ) );
     }
 
-    
-    
     // write posts to list
     $list = array();
-
     foreach ( (array) $post_list as $post_item) {  
         $list[$post_item->ID] = $post_item->post_title;
     }
