93d6750a4d8c5be071310269d4b07169ba19831f
Tobias Pörtner <t.poertner@outlook.com>
2021-04-05 16:33:27 +0200

    fix: messed up rewrite rules in htaccess

--- includes/smartphoniker-mod-rewrite-rules.php
+++ includes/smartphoniker-mod-rewrite-rules.php
@@ -21,7 +21,8 @@ add_filter('mod_rewrite_rules', 'smartphoniker_add_custom_htaccess_content');
  */
 function smartphoniker_add_custom_htaccess_content( string $rules ) : string {
     $hide_author_page = <<<EOD
-        \n\n # BEGIN My Added Content
+        \n\n
+        # BEGIN My Added Content
         # Hide author page
         RewriteCond %{REQUEST_URI} !^/wp-admin [NC]
         RewriteCond %{QUERY_STRING} author=\d
@@ -29,7 +30,7 @@ function smartphoniker_add_custom_htaccess_content( string $rules ) : string {
         # END My Added Content\n
         EOD;    
     $position = strpos( $rules, 'RewriteEngine On') + 16;
-    $new_rules = substr_replace( $rules, $position, $position, 0);
+    $new_rules = substr_replace( $rules, $hide_author_page, $position, 0);
     
     return $new_rules;
 }
