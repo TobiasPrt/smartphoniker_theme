2c5132fc4426900a28a204894e94c63b94e68b96
Tobias Pörtner <t.poertner@outlook.com>
2021-04-09 10:56:36 +0200

    undo changes

--- template-parts/component-hero.php
+++ template-parts/component-hero.php
@@ -33,7 +33,7 @@ $manufacturers = get_categories( array(
         width="1"
         height="1"
         class="hero__image"
-        src="<?php echo get_template_directory_uri(); ?>/assets/images/hero_image_phonex600.png"
+        src="<?php echo get_template_directory_uri(); ?>/assets/images/hero_image_phone.svg"
         alt="Nachhaltiges Handy"
     />
 
