b2100a42277f6c5b1c4ce9ae66de57601b93d3bd
Tobias Pörtner <t.poertner@outlook.com>
2021-05-05 12:13:54 +0200

    adds reusable block functionality to admin menu

--- includes/smartphoniker-edit-admin-menu.php
+++ includes/smartphoniker-edit-admin-menu.php
@@ -4,6 +4,7 @@
  * 
  * @package Smartphoniker
  * @since 1.0.0
+ * @since 1.1.0 new function for adding entries to admin menu
  */
 add_action( 'admin_menu', 'smartphoniker_edit_admin_menu' );
 
@@ -11,10 +12,12 @@ add_action( 'admin_menu', 'smartphoniker_edit_admin_menu' );
 /**
  * Calls functions for customizing admin menu.
  * 
- * @since 1.0.0.
+ * @since 1.0.0
+ * @since 1.0.0 call to _add_menu_pages()
  */
 function smartphoniker_edit_admin_menu() {
     smartphoniker_remove_menu_pages();
+    smartphoniker_add_menu_pages();
 }
 
 
@@ -27,4 +30,22 @@ function smartphoniker_remove_menu_pages() {
   remove_menu_page( 'edit.php' ); //Posts
   remove_menu_page( 'edit-comments.php' ); //Comments
   remove_menu_page( 'users.php' ); //Users
+}
+
+
+/**
+ * Adds entries to admin menu
+ *
+ * @since 1.1.0
+ */
+function smartphoniker_add_menu_pages() {
+  add_menu_page(
+			esc_html__( 'Reusable Blocks', 'reusable-blocks-admin-menu-option' ), // page title
+			esc_html__( 'Reusable Blocks', 'reusable-blocks-admin-menu-option' ), // menu title
+			'read', // capability of user to see this
+			'edit.php?post_type=wp_block', // slug (link)
+			'', // function to be called to output (here: empty, page outputs itself)
+			'dashicons-layout', // icon
+			20 // positon: below pages
+		);
 }
