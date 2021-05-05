d24d5368271cd554286f91f6689f40105ecf89a0
Tobias Pörtner <t.poertner@outlook.com>
2021-05-05 12:15:33 +0200

    comment

--- includes/smartphoniker-set-allowed-block-types.php
+++ includes/smartphoniker-set-allowed-block-types.php
@@ -12,6 +12,7 @@ add_filter( 'allowed_block_types', 'smartphoniker_allowed_block_types' );
  * Returns only allowed Gutenberg Block types.
  * 
  * @since 1.0.0
+ * @since 1.1.0 allowes core/block
  * 
  * @return array $allowed_block_types Array of all allowed Gutenberg Block types.
  */
