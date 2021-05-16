9874f9e502cfcf302d6e85105dab96786d6b25ba
Tobias Pörtner <t.poertner@outlook.com>
2021-05-16 11:42:33 +0200

    edited post meta for device post type

--- includes/post_meta/post-meta-device.php
+++ includes/post_meta/post-meta-device.php
@@ -14,24 +14,23 @@ use Carbon_Fields\Field;
  * Creates post meta for device post type.
  * 
  * @since 1.0.0
+ * @since 1.1.0 added bestseller and image option
  */
 (function() {
     Container::make( 'post_meta', _( 'Geräte Einstellungen' ) )
         ->where( 'post_type', '=', 'device' )
         ->set_context( 'normal' )
         ->add_fields( array(
-            Field::make( 'text', 'price', __( 'Günstigster Reparaturpreis für dieses Modell angeben' ) )
-                ->set_help_text( 'Format: 79€ (ohne Kommma, mit € Symbol)' ),
-            Field::make( 'set', 'repairs', __( 'Für dieses Modell verfügbare Reparaturen' ) )
-                ->set_options( array(
-                    'display' => 'Displayrepatur',
-                    'back' => 'Backcover reparieren',
-                    'front_cam' => 'Frontkamera',
-                    'main_cam' => 'Rückkamera',
-                    'battery' => 'Akkutausch',
-                    'misc' => 'sonstige Kleinteilreparatur'
-                ) ),
-            Field::make( 'image', 'device_image', __( 'Geräte-Bild' ) ),
+            Field::make( 'checkbox', 'is_bestseller', __( 'Gerät ist ein Top-Modell für die Darstellung in anderen Blocks.' ) )
+                ->set_option_value( 'yes' ),
+            Field::make( 'image', 'device_image', __( 'Geräte-Bild' ) )
+                ->set_conditional_logic( array(
+                    array(
+                        'field' => 'is_bestseller',
+                        'value' => true
+                    )
+                ))
+                ->set_required( true ),
             Field::make( 'text', 'link', __( 'Link zum Smartphoniker Shop' ) )
                 ->set_required( true ),
     ) );
