3a9382374c69f0ed34e7f2c15f3e46067e872a4d
Tobias Pörtner <t.poertner@outlook.com>
2021-05-23 09:52:39 +0200

    naming fix

--- template-parts/component-services-list-2.php
+++ template-parts/component-services-list-2.php
@@ -13,14 +13,16 @@
     <!-- 1 Column per service -->
     <?php foreach ( (array) $args['services'] as $service ): ?>
 
+        <?php var_dump($service); ?>
+
         <div class="columns-2__column">
-                <?php echo wp_get_attachment_image( get_post_meta( intval( $service['services'] ), '_icon' )[0], 'thumbnail', false, array( 'class' => 'columns-2__icon' ) ); ?>
+                <?php echo wp_get_attachment_image( get_post_meta( intval( $service['service_id'] ), '_icon' )[0], 'thumbnail', false, array( 'class' => 'columns-2__icon' ) ); ?>
                 <div class="columns-2__contentwrapper">
-                    <a class="columns-2__heading columns-2__heading--link" href="<?php echo get_permalink( $service['services'] ); ?>">
-                            <?php echo get_post_field( 'post_title', intval( $service['services'] ) ); ?>
+                    <a class="columns-2__heading columns-2__heading--link" href="<?php echo get_permalink( $service['service_id'] ); ?>">
+                            <?php echo get_post_field( 'post_title', intval( $service['service_id'] ) ); ?>
                     </a>
                     <p class="columns-2__text">
-                        <?php echo get_post_meta( intval( $service['services'] ), '_description' )[0]; ?>
+                        <?php echo get_post_meta( intval( $service['service_id'] ), '_description' )[0]; ?>
                     </p>
                 </div>
             </div>
