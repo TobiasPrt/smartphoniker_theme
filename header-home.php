269e71114b1379c4522fa28b1f511137fa8d572d
Tobias Pörtner <t.poertner@outlook.com>
2021-07-05 16:25:11 +0200

    Move header button to the left

--- header-home.php
+++ header-home.php
@@ -68,6 +68,11 @@
                         alt="Smartphoniker Logo" />
                 </a>
 
+                <!-- Button for sending in device -->
+                <?php if (carbon_get_theme_option('logo_button_is_enabled') == true): ?>
+                    <a href="<?php echo carbon_get_theme_option('sendin_form_link') ?>" class="button button--white nav__button"><?php echo carbon_get_theme_option('sendin_form_button_text') ?></a>
+                <?php endif; ?>
+
                 
 
 
@@ -95,10 +100,7 @@
                         alt="Smartphoniker anrufen" />
                 </a>
 
-                <!-- Button for sending in device -->
-                <?php if (carbon_get_theme_option('logo_button_is_enabled') == true): ?>
-                    <a href="<?php echo carbon_get_theme_option('sendin_form_link') ?>" class="button button--white nav__button"><?php echo carbon_get_theme_option('sendin_form_button_text') ?></a>
-                <?php endif; ?>
+                
 
 
                 <!-- Hamburger Menu Icon -->
